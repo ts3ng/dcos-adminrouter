6fab85c71697682873695cb284dc5fda23bdfa30
tseng <tseng@adobe.com>
2017-06-21 14:38:19 -0700

    Marathon Auth Support

---
+++ extra/src/master/util.lua
@@ -0,0 +1,42 @@
+local cjson_safe = require "cjson.safe"
+
+local util = {}
+local b='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+
+function util.mesos_dns_get_srv(framework_name)
+    local res = ngx.location.capture(
+        "/mesos_dns/v1/services/_" .. framework_name .. "._tcp.marathon.mesos")
+
+    if res.truncated then
+        -- Remote connection dropped prematurely or timed out.
+        ngx.log(ngx.ERR, "Request to Mesos DNS failed.")
+        return nil
+    end
+    if res.status ~= 200 then
+        ngx.log(ngx.ERR, "Mesos DNS response status: " .. res.status)
+        return nil
+    end
+
+    local records, err = cjson_safe.decode(res.body)
+    if not records then
+        ngx.log(ngx.ERR, "Cannot decode JSON: " .. err)
+        return nil
+    end
+    return records
+end
+
+function util.base64encode(data)
+    return ((data:gsub('.', function(x) 
+        local r,b='',x:byte()
+        for i=8,1,-1 do r=r..(b%2^i-b%2^(i-1)>0 and '1' or '0') end
+        return r;
+    end)..'0000'):gsub('%d%d%d?%d?%d?%d?', function(x)
+        if (#x < 6) then return '' end
+        local c=0
+        for i=1,6 do c=c+(x:sub(i,i)=='1' and 2^(6-i) or 0) end
+        return b:sub(c+1,c+1)
+    end)..({ '', '==', '=' })[#data%3+1])
+end
+
+
+return util
